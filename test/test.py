75cb139c0da9a889e976762ed8522579ba9ef7c7
sej3939 <sj3939@nyu.edu>
2024-12-11 18:22:40 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ def right_shift_and_insert(arr, insertion):
 
 def mult_and_sum(arr1, arr2):
     sum = 0
-    for i in range(arr1):
+    
         sum += arr1[i] * arr2[i]
     return sum
 
@@ -26,26 +26,8 @@ async def test_project(dut):
     
     TAPS = [75, 0, 95, 95, 0, 75]
     input_mem = [0, 0, 0, 0, 0, 0]
-    input_seq = [0, 0, 0, 0, 0, 0]
+    input_seq = [1, 1, 0, 0, 0, 0]
 
     # Set the input values you want to test
-    for i in input_seq:
-        
-        
-        for j in input_seq:
-            dut.input_fir.value = j
-            
-            # Wait for one clock cycle to see the output values
-            await ClockCycles(dut.clk, 10)
     
-            input_mem = right_shift_and_insert(input_mem, input_seq[j])
-            fir_out = mult_and_sum(input_mem, TAPS)  
-    
-            # The following assersion is just an example of how to check the output values.
-            # Change it to match the actual expected output of your module:
-            dut._log.info(f"value of outputs are: {dut.output_fir.value}.")
-            # If these passes don't work, fail the program and show what you failed.
-        
-            # Keep testing the module by changing the input values, waiting for
-            # one or more clock cycles, and asserting the expected output values.
 
