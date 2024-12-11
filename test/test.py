f0ca9ac2491d9fe12d51ad7c57306322f07958c5
sej3939 <sj3939@nyu.edu>
2024-12-11 17:12:49 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,6 +5,14 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+def right_shift_and_insert(arr, insertion):
+    return [insertion] + arr[:-1]  # Add insertion at the start, drop the last element
+
+def mult_and_sum(arr1, arr2):
+    sum = 0
+    for i in range(arr1):
+        sum += arr1[i] * arr2[i]
+    return sum
 
 @cocotb.test()
 async def test_project(dut):
@@ -15,17 +23,25 @@ async def test_project(dut):
     cocotb.start_soon(clock.start())
 
     dut._log.info("Test project behavior")
+    
+    TAPS = [75, 0, -95, 0 , 37, 0, -60, -60, 0, 37, 0, -95, 0, 75]
+    input_mem = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    input_seq = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
     # Set the input values you want to test
+    for i in range(input_Seq):
+        input_mem = right_shift_and_insert(input_mem, input_seq[i])
+        dut.input_fir.value = input_mem
+        fir_out = mult_and_sum(input_mem, TAPS)    
+        
+        # Wait for one clock cycle to see the output values
+        await ClockCycles(dut.clk, 10)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 10)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-
-    # If these passes don't work, fail the program and show what you failed.
+        # The following assersion is just an example of how to check the output values.
+        # Change it to match the actual expected output of your module:
+        dut._log.info(f"value of outputs are: {dut.output_fir.value}.")
+        # If these passes don't work, fail the program and show what you failed.
     
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        # Keep testing the module by changing the input values, waiting for
+        # one or more clock cycles, and asserting the expected output values.
 
