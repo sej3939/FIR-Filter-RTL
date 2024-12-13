155600118b16cf999e98251679ede581e269456e
sej3939 <sj3939@nyu.edu>
2024-12-12 22:29:19 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -49,11 +49,8 @@ async def test_project(dut):
         dut.input_fir.value = i # Provide the input to the DUT
 
         if time == 0:
-            await ClockCycles(dut.clk, 1)
-            time += 1
-            while dut.y_trio.value == 0:
-                await ClockCycles(dut.clk, 1)
-                time += 1
+            await ClockCycles(dut.clk, 44)
+            time += 44
         while dut.y_trio.value == 0:
             await ClockCycles(dut.clk, 1)
             time += 1
