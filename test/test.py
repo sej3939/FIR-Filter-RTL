149cbd2fa622f989b7e26f625251176f16f8c9a3
sej3939 <sj3939@nyu.edu>
2024-12-12 20:36:51 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -46,7 +46,7 @@ async def test_project(dut):
         expected_output = fir(i)
 
         dut.input_fir.value = i # Provide the input to the DUT
-        await ClockCycles(dut.clk, 100)
+        await ClockCycles(dut.clk, 44)
         # Print the result for verification
         dut._log.info(f"i: {i} - Expected y: {expected_output} - DUT y: {dut.output_fir.value.integer} - y_trio: {dut.y_trio.value} - x_trio: {dut.x_trio.value}.")
 
